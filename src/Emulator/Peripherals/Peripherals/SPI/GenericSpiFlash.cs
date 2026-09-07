f77afd52d137f37a5ab37411bef24b4102a98130
crispasr integration <crispasr-dev@localhost>
2026-09-07 11:03:43 +0000

    spi: verify generic flash for W25Q256 commands

--- src/Emulator/Peripherals/Peripherals/SPI/GenericSpiFlash.cs
+++ src/Emulator/Peripherals/Peripherals/SPI/GenericSpiFlash.cs
@@ -25,7 +25,7 @@ namespace Antmicro.Renode.Peripherals.SPI
             bool writeStatusCanSetWriteEnable = true, byte extendedDeviceId = DefaultExtendedDeviceID,
             byte deviceConfiguration = DefaultDeviceConfiguration, byte remainingIdBytes = DefaultRemainingIDBytes,
             // "Sector" here is the largest erasable memory unit. It's also named "block" by many flash memory vendors.
-            int sectorSizeKB = DefaultSectorSizeKB)
+            int sectorSizeKB = DefaultSectorSizeKB, byte? secondaryStatusRegisterReadCommand = null)
         {
             if(!Misc.IsPowerOfTwo((ulong)underlyingMemory.Size))
             {
@@ -64,6 +64,7 @@ namespace Antmicro.Renode.Peripherals.SPI
             this.remainingIdBytes = remainingIdBytes;
             this.extendedDeviceId = extendedDeviceId;
             this.deviceConfiguration = deviceConfiguration;
+            this.secondaryStatusRegisterReadCommand = secondaryStatusRegisterReadCommand;
 
             deviceData = GetDeviceData();
 
@@ -172,7 +173,9 @@ namespace Antmicro.Renode.Peripherals.SPI
             {
                 return;
             }
-            underlyingMemory.WriteByte(position, val);
+            // Programming NOR flash can only clear bits. An erase operation is
+            // required before a cleared bit can become set again.
+            underlyingMemory.WriteByte(position, (byte)(underlyingMemory.ReadByte(position) & val));
         }
 
         protected bool TryVerifyWriteToMemory(out long position)
@@ -222,6 +225,7 @@ namespace Antmicro.Renode.Peripherals.SPI
             switch(command)
             {
             case Commands.FastRead:
+            case Commands.FastRead4byte:
             case Commands.ReadSerialFlashDiscoveryParameter:
                 return 1;
             default:
@@ -234,6 +238,12 @@ namespace Antmicro.Renode.Peripherals.SPI
             currentOperation.Operation = DecodedOperation.OperationType.None;
             currentOperation.State = DecodedOperation.OperationState.HandleCommand;
             currentOperation.DummyBytesRemaining = GetDummyBytes((Commands)firstByte);
+            if(secondaryStatusRegisterReadCommand.HasValue && firstByte == secondaryStatusRegisterReadCommand.Value)
+            {
+                currentOperation.Operation = DecodedOperation.OperationType.ReadRegister;
+                currentOperation.Register = (uint)Register.SecondaryStatus;
+                return;
+            }
             switch(firstByte)
             {
             case (byte)Commands.ReadID:
@@ -328,8 +338,16 @@ namespace Antmicro.Renode.Peripherals.SPI
                 break;
             case (byte)Commands.BulkErase:
             case (byte)Commands.ChipErase:
-                this.Log(LogLevel.Noisy, "Performing bulk/chip erase");
-                EraseChip();
+                if(enable.Value)
+                {
+                    this.Log(LogLevel.Noisy, "Performing bulk/chip erase");
+                    EraseChip();
+                    enable.Value = false;
+                }
+                else
+                {
+                    this.Log(LogLevel.Error, "Chip erase operation is disabled.");
+                }
                 break;
             case (byte)Commands.SubsectorErase4byte4kb:
                 currentOperation.Operation = DecodedOperation.OperationType.Erase;
@@ -469,6 +487,10 @@ namespace Antmicro.Renode.Peripherals.SPI
                 // The documentation states that at least 1 byte will be read
                 // If more than 1 byte is read, the same byte is returned
                 return statusRegister.Read();
+            case Register.SecondaryStatus:
+                // Meanings differ between device families. A configured
+                // second status-register opcode defaults to no asserted flags.
+                return 0;
             case Register.FlagStatus:
                 // The documentation states that at least 1 byte will be read
                 // If more than 1 byte is read, the same byte is returned
@@ -706,6 +728,7 @@ namespace Antmicro.Renode.Peripherals.SPI
         private readonly byte remainingIdBytes;
         private readonly byte extendedDeviceId;
         private readonly byte deviceConfiguration;
+        private readonly byte? secondaryStatusRegisterReadCommand;
         private const byte EmptySegment = 0xff;
         private const byte DeviceGeneration = 0x1;      // 2nd generation
         private const byte DefaultRemainingIDBytes = 0x10;
@@ -850,6 +873,7 @@ namespace Antmicro.Renode.Peripherals.SPI
             NonVolatileConfiguration,
             VolatileConfiguration,
             EnhancedVolatileConfiguration,
+            SecondaryStatus,
             FirstNonstandardRegister,
         }
 
---
+++ src/Emulator/Peripherals/Test/PeripheralsTests/GenericSpiFlashW25Q256Tests.cs
@@ -0,0 +1,159 @@
+//
+// Copyright (c) 2026 CrispStrobe
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Peripherals.Memory;
+using Antmicro.Renode.Peripherals.SPI;
+
+using NUnit.Framework;
+
+namespace Antmicro.Renode.PeripheralsTests
+{
+    [TestFixture]
+    public class GenericSpiFlashW25Q256Tests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            machine = new Machine();
+            memory = new MappedMemory(machine, FlashSize);
+            flash = new GenericSpiFlash(memory, 0xEF, 0x40, 0x19,
+                writeStatusCanSetWriteEnable: false, sectorSizeKB: 64,
+                secondaryStatusRegisterReadCommand: 0x35);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            memory.Dispose();
+            machine.Dispose();
+        }
+
+        [Test]
+        public void ShouldReportW25Q256IdentityAndIdleStatus()
+        {
+            AssertCommand(0x9F, new byte[] { 0xEF, 0x40, 0x19 });
+            AssertCommand(0x05, new byte[] { 0x00 });
+            AssertCommand(0x35, new byte[] { 0x00 });
+        }
+
+        [Test]
+        public void ShouldTrackAndConsumeWriteEnableLatch()
+        {
+            Execute(0x06);
+            AssertCommand(0x05, new byte[] { 0x02 });
+
+            Execute(0x12, AddressBytes(TestAddress), new byte[] { 0xA5 });
+
+            AssertCommand(0x05, new byte[] { 0x00 });
+            Assert.AreEqual(0xA5, memory.ReadByte(TestAddress));
+        }
+
+        [Test]
+        public void ShouldFastReadAcrossFourByteAddressWithDummyCycle()
+        {
+            memory.WriteBytes(TestAddress, new byte[] { 0x11, 0x22, 0x33 });
+
+            Begin(0x0C, AddressBytes(TestAddress), new byte[] { 0xFF });
+            var result = Clock(3);
+            flash.FinishTransmission();
+
+            CollectionAssert.AreEqual(new byte[] { 0x11, 0x22, 0x33 }, result);
+        }
+
+        [Test]
+        public void ShouldApplyNorProgrammingRulesAndRequireWriteEnable()
+        {
+            memory.WriteByte(TestAddress, 0x0F);
+            Execute(0x12, AddressBytes(TestAddress), new byte[] { 0xF0 });
+            Assert.AreEqual(0x0F, memory.ReadByte(TestAddress));
+
+            Execute(0x06);
+            Execute(0x12, AddressBytes(TestAddress), new byte[] { 0xF0 });
+            Assert.AreEqual(0x00, memory.ReadByte(TestAddress));
+        }
+
+        [Test]
+        public void ShouldEraseFourByteSectorAndBlockAndRemainImmediatelyIdle()
+        {
+            memory.WriteByte(TestAddress, 0x00);
+            Execute(0x06);
+            Execute(0x21, AddressBytes(TestAddress));
+            Assert.AreEqual(0xFF, memory.ReadByte(TestAddress));
+            AssertCommand(0x05, new byte[] { 0x00 });
+
+            memory.WriteByte(TestAddress, 0x00);
+            Execute(0x06);
+            Execute(0xDC, AddressBytes(TestAddress));
+            Assert.AreEqual(0xFF, memory.ReadByte(TestAddress));
+            AssertCommand(0x05, new byte[] { 0x00 });
+        }
+
+        [Test]
+        public void ShouldRequireWriteEnableForChipErase()
+        {
+            memory.WriteByte(TestAddress, 0x00);
+            Execute(0xC7);
+            Assert.AreEqual(0x00, memory.ReadByte(TestAddress));
+
+            Execute(0x06);
+            Execute(0xC7);
+            Assert.AreEqual(0xFF, memory.ReadByte(TestAddress));
+            AssertCommand(0x05, new byte[] { 0x00 });
+        }
+
+        private void AssertCommand(byte command, byte[] expected)
+        {
+            Begin(command);
+            CollectionAssert.AreEqual(expected, Clock(expected.Length));
+            flash.FinishTransmission();
+        }
+
+        private void Execute(byte command, params byte[][] segments)
+        {
+            Begin(command, segments);
+            flash.FinishTransmission();
+        }
+
+        private void Begin(byte command, params byte[][] segments)
+        {
+            flash.Transmit(command);
+            foreach(var segment in segments)
+            {
+                foreach(var value in segment)
+                {
+                    flash.Transmit(value);
+                }
+            }
+        }
+
+        private byte[] Clock(int count)
+        {
+            var result = new byte[count];
+            for(var i = 0; i < count; ++i)
+            {
+                result[i] = flash.Transmit(0xFF);
+            }
+            return result;
+        }
+
+        private static byte[] AddressBytes(long address)
+        {
+            return new byte[]
+            {
+                (byte)(address >> 24), (byte)(address >> 16),
+                (byte)(address >> 8), (byte)address,
+            };
+        }
+
+        private Machine machine;
+        private MappedMemory memory;
+        private GenericSpiFlash flash;
+
+        private const int FlashSize = 32 * 1024 * 1024;
+        private const int TestAddress = 0x01001010;
+    }
+}
