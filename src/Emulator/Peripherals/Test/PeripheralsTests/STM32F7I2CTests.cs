9e0bfb9d58ea142ceedf43ed60df037484d3fd48
crispasr integration <crispasr-dev@localhost>
2026-09-08 18:12:26 +0000

    test(stm32): unregister I2C fixture machine

--- src/Emulator/Peripherals/Test/PeripheralsTests/STM32F7I2CTests.cs
+++ src/Emulator/Peripherals/Test/PeripheralsTests/STM32F7I2CTests.cs
@@ -37,7 +37,7 @@ namespace Antmicro.Renode.PeripheralsTests
         [TearDown]
         public void TearDown()
         {
-            machine.Dispose();
+            EmulationManager.Instance.CurrentEmulation.RemoveMachine(machine);
         }
 
         [Test]
